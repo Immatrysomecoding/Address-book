10e01c28ceae7e9f4cbd43d7a331903fc5bffa68
Immatrysomecoding <91767814+Immatrysomecoding@users.noreply.github.com>
2024-04-07 14:17:34 +0700

    Update address_book.cpp

--- address_book.cpp
+++ address_book.cpp
@@ -1,13 +1,3 @@
-/********************
- * <copyright information>
- * AUTHOR: Michael Mangrobang
- * COURSE: ICS 212 Program Structure
- * INSTRUCTOR: Ravi Narayan
- * DATE: 21 November 2015
- * REVISIONS: 08 June 2018
- * FILE: address_book.cpp
- * DESC: Source file that contains the address book functions
- ********************/
 #include <iostream>
 #include <string>
 #include "record.h"
