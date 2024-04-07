1076a4a49eeaeb6f891178a07264002b0f00cf3d
Immatrysomecoding <91767814+Immatrysomecoding@users.noreply.github.com>
2024-04-07 14:20:02 +0700

    Update project_02_display_messages.cpp

--- project_02_display_messages.cpp
+++ project_02_display_messages.cpp
@@ -1,13 +1,3 @@
-/********************
- * <copyright information>
- * AUTHOR: Michael Mangrobang
- * COURSE: ICS 212 Program Structure
- * INSTRUCTOR: Ravi Narayan
- * DATE CREATED: 21 November 2015
- * REVISIONS: 08 June 2018
- * FILE: project_02_display_messages.cpp
- * DESC: Source file that contains majority of the interface
- ********************/
 #include "project_02_prototypes.h"
 #include <iostream>
 
