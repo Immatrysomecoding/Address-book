bdd459b19bb86f318df0967ddd9aaee504e233ab
Immatrysomecoding <91767814+Immatrysomecoding@users.noreply.github.com>
2024-04-07 14:20:15 +0700

    Update record.h

--- record.h
+++ record.h
@@ -1,13 +1,3 @@
-/********************
- * <copyright information>
- * AUTHOR: Michael (Micky) Mangrobang
- * COURSE: ICS 212 Program Structure
- * INSTRUCTOR: Ravi Narayan
- * DATE: 21 November 2015
- * REVISIONS: 08 June 2018
- * FILE: record.h
- * DESC: Header file for record, given by Ravi Naryan, edited by me
- ********************/
 #include <string>
 #ifndef RECORD_H
 #define RECORD_H
